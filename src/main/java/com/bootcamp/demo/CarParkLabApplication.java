0e69b2f7b5eb1d153dc2b4cc7aa82ec40d6f0599
Ahmet Kaya <kayaseyitahmet42@gmail.com>
2020-04-05 00:23:09 +0100

    added int x;

--- src/main/java/com/bootcamp/demo/CarParkLabApplication.java
+++ src/main/java/com/bootcamp/demo/CarParkLabApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarParkLabApplication {
+	int x;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarParkLabApplication.class, args);
